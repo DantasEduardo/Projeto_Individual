056539d591fecc7ca7363e826bfe9baa620928f8
DantasEduardo <eduardo.oliveira@bandtec.com.br>
2021-05-16 22:50:00 -0300

    API de cadastro e login funcionando

--- BD/script_bd.sql
+++ BD/script_bd.sql
@@ -1,32 +1,14 @@
-create database ProjetoIndividual;
-use ProjetoIndividual;
+create database bushido;
+use bushido;
 
 create table Cadastro(
 	idCadastro int primary key auto_increment,
     nome varchar(45),
+    cpf char(11) unique key,
+    cep char(8),
     idade date,
     arteMarcial varchar(45),
-    CEP char(8) );
-    
-create table Usuario(
-	idUsuario int primary key auto_increment,
-    usuario varchar(45),
-    senha varchar(45),
-    fkCadastro int,
-    foreign key (fkCadastro) references Cadastro(idCadastro)
-);
-
-create table PlanoTreino(
-	idPlanoTreino int primary key auto_increment,
-    nomeTreino varchar(45),
-    descricao varchar(90),
-    fkUsuario int,
-    foreign key (fkUsuario) references Usuario(idUsuario)
-);
-
-create table Comentarios(
-	idComentario int primary key auto_increment,
-    descricaoComentario varchar(200),
-    fkUsuario int,
-    foreign key (fkUsuario) references Usuario(idUsuario)
-);
+    email varchar(45),
+    login varchar(45),
+    senha varchar(45)
+    );
